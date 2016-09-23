89c32d81c8fc4552291f62a1ed5bd4101bf5403c
hrishiishere <hrishikeshsunder39@gmail.com>
2016-09-23 21:59:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.get('/', function (req, res) {
 app.get('/article-one', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+
+var counter = 0;
+app.get('/counter', function(req , res){
+   counter = counter + 1 ;
+   res.send(counter.toString());
+});
+
 app.get('/article-two', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
