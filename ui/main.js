1defb2b272a0d61284b25c1bf6d337f63f2334b6
hrishiishere <hrishikeshsunder39@gmail.com>
2016-09-23 20:17:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+console.log('by hrishi!');
+var img= document.getElementById('image');
+var marginleft='0';
+function moveright()
+{
+    marginleft = marginleft + 1 ;
+    img.style.marginleft = marginleft + 'px';
+}
+img.onclick = function()
+{
+    var interval = setinterval(moveright, 50);
+};
